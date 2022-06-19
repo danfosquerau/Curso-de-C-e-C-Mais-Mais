7000847fc1e6012a65b8eda4c85cc263fc8ebf8c
Daniel Klostermann Fosquerau <danielkfosquerau@gmail.com>
2022-06-19 20:41:28 -0300

    Initial commit

---
+++ C/Teste.c
@@ -0,0 +1,10 @@
+//Biblioteca para ler comandos de entradas e sai­das
+#include <stdio.h>
+//Biblioteca que possui todos as funções básicas do C
+//envolvendo alocação de memória, controle de processos, conversões e outras...
+#include <stdlib.h>
+
+void main(){
+    //Imprimir "Hello, World!" na tela
+    printf("Hello, World!");
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Curso de C e C++
+
+Link do curso na Udemy  
+[Curso Completo de Linguagem C e C++ - Iniciante Ao Avançado](https://www.udemy.com/course/curso-completo-de-linguagem-c-iniciante-ao-avancado/)
